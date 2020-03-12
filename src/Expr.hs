c5c852ac36935ee346a1c42ee8c186d751bc9073
Vasiliy Lupuleac <v_lupuleac@mail.ru>
2020-03-13 02:29:40 +0300

    Fix imports and uberExpr

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,8 +1,8 @@
 module Expr where
 
+import           Control.Applicative (Alternative (..))
 import           AST         (AST (..), Operator (..))
-import           Combinators (Parser (..), Result (..), bind', elem', fail',
-                              fmap', satisfy, some', success)
+import           Combinators
 import           Data.Char   (digitToInt, isDigit)
 
 data Associativity
@@ -16,31 +16,46 @@ uberExpr :: Monoid e
          -> Parser e i ast -- парсер для элементарного выражения
          -> (op -> ast -> ast -> ast) -- функция для создания абстрактного синтаксического дерева для бинарного оператора
          -> Parser e i ast
-uberExpr = error "uberExpr undefined"
+uberExpr ops term ast = foldr f term ops where
+  opFoldl a (op, b) = ast op a b
+  opFoldr (a, op) b = ast op a b
+  f (op, NoAssoc) expr    = (\a op b -> ast op a b) <$> expr <*> op <*> expr <|> expr
+  f (op, LeftAssoc) expr  = (uncurry $ foldl opFoldl) <$> sepBy1l op expr
+  f (op, RightAssoc) expr = (uncurry. flip $ foldr opFoldr) <$> sepBy1r op expr
 
 -- Парсер для выражений над +, -, *, /, ^ (возведение в степень)
 -- с естественными приоритетами и ассоциативностью над натуральными числами с 0.
 -- В строке могут быть скобки
+
 parseExpr :: Parser String String AST
-parseExpr = error "parseExpr undefined"
+parseExpr = uberExpr [(opParser '+' <|> opParser '-', LeftAssoc),
+                      (opParser '*' <|> opParser '/', LeftAssoc),
+                      (opParser '^', RightAssoc)]
+                      (Num <$> parseNum <|> symbol '(' *> parseExpr <* symbol ')')
+                      BinOp
+                      
 
 -- Парсер для натуральных чисел с 0
 parseNum :: Parser String String Int
-parseNum = foldl (\acc d -> 10 * acc + digitToInt d) 0 `fmap'` go
+parseNum = foldl (\acc d -> 10 * acc + digitToInt d) 0 <$> go
   where
     go :: Parser String String String
-    go = some' (satisfy isDigit)
+    go = some (satisfy isDigit)
 
 -- Парсер для операторов
 parseOp :: Parser String String Operator
-parseOp = elem' `bind'` toOperator
+parseOp = elem' >>= toOperator
+
+opParser :: Char -> Parser String String Operator
+opParser x = symbol x >>= toOperator
 
 -- Преобразование символов операторов в операторы
 toOperator :: Char -> Parser String String Operator
-toOperator '+' = success Plus
-toOperator '*' = success Mult
-toOperator '-' = success Minus
-toOperator '/' = success Div
+toOperator '+' = pure Plus
+toOperator '*' = pure Mult
+toOperator '-' = pure Minus
+toOperator '/' = pure Div
+toOperator '^' = pure Pow
 toOperator _   = fail' "Failed toOperator"
 
 evaluate :: String -> Maybe Int
@@ -55,4 +70,5 @@ compute (BinOp Plus x y)  = compute x + compute y
 compute (BinOp Mult x y)  = compute x * compute y
 compute (BinOp Minus x y) = compute x - compute y
 compute (BinOp Div x y)   = compute x `div` compute y
+compute (BinOp Pow x y)   = compute x ^ compute y
 
