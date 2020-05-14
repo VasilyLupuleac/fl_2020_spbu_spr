9aa00989add4c41e11ef94fcb42a13318ad474b7
Vasiliy Lupuleac <v_lupuleac@mail.ru>
2020-05-14 20:31:11 +0300

    Add solutions to problems 1-3

--- src/Regexp.hs
+++ src/Regexp.hs
@@ -13,8 +13,21 @@ data Regexp = Empty
 match :: Regexp -> String -> Bool
 match r s = nullable (foldl (flip derivative) r s)
 
+nu :: Regexp -> Regexp -> Regexp
+nu r | nullable r = id
+     | otherwise  = const Empty
+
 derivative :: Char -> Regexp -> Regexp
-derivative = error "derivative not implemented"
+derivative c (Char c') | c == c'   = Epsilon
+                       | otherwise = Empty
+derivative c (Seq r r') = Alt (Seq (derivative c r) r') (nu r (derivative c r'))
+derivative c (Alt r r')  = Alt (derivative c r) (derivative c r')
+derivative c (Star r)    = Seq (derivative c r) (Star r)
+derivative _ _           = Empty
 
 nullable :: Regexp -> Bool
-nullable = error "nullable not implemented"
+nullable (Char _)   = False
+nullable Empty      = False
+nullable (Seq r r') = (nullable r) && (nullable r')
+nullable (Alt r r') = (nullable r) || (nullable r')
+nullable _          = True
